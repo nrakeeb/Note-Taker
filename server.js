6067f8c68f080be7027e2d8158428492c9d23928
Nouha Rakeeb <Nouha_Rakeeb@hotmail.co.uk>
2021-09-08 17:04:36 +0100

    feat: added GET routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const noteData = require('./db/db.json');
+const path = require('path');
 
 const PORT = 3001;
 const app = express();
@@ -10,9 +11,15 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-app.get('/notes', (req, res) => res.json(noteData));
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
-//app.delete('/deleteNote', (req, res) => )
+app.get('/api/notes', (req, res) => res.json(noteData));
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
