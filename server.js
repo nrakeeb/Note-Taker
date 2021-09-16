98787c11e859f5ea84d2376c046ebb772733e01f
Nouha Rakeeb <Nouha_Rakeeb@hotmail.co.uk>
2021-09-16 17:23:56 +0100

    feat: added uuid helper to generate random ids for notes

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const noteData = require('./db/db.json');
+const uuid = require('./helpers/uuid');
 const path = require('path');
 
 const PORT = 3001;
 const app = express();
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
@@ -16,6 +17,10 @@ app.get('/notes', (req, res) =>
 );
 
 app.get('/api/notes', (req, res) => res.json(noteData));
+
+app.post('/api/notes', (req, res) => console.log("success!"))
+
+//app.delete('/api/notes/:id', (req, res) => )
 
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
