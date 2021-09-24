af110d60e838d95b2d188a77e5a6919ea920d0bb
Nouha Rakeeb <Nouha_Rakeeb@hotmail.co.uk>
2021-09-24 15:16:49 +0100

    feat: added post route and readAndAppend helper function

---
+++ helpers/readAndAppend.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+const writeToFile = (destination, content) =>
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+        err ? console.error(err) : console.info(`\nData written to ${destination}`)
+    );
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedNotes = JSON.parse(data);
+            parsedNotes.push(content);
+            writeToFile(file, parsedNotes)
+        }
+    });
+};
+
+module.exports = readAndAppend;
--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const readAndAppend = require('../helpers/readAndAppend')
 
 const noteData = require('../db/db.json');
 const uuid = require('../helpers/uuid');
 
 router.get('/', (req, res) => res.json(noteData));
 
-router.post('/', (req, res) => console.log("success!"))
+router.post('/', (req, res) => {
+
+const {title, text} = req.body;
+
+if (title && text) {
+    const newNote = {
+        title,
+        text,
+        id: uuid()
+    };
+
+    readAndAppend(newNote, './db/db.json')
+
+    const response = {
+        status: 'success',
+        body: newNote
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+} else {
+    res.status(500).json('Error')
+}
+});
 
 //router.delete('/api/notes/:id', (req, res) => )
 
