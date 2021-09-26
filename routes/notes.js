0cb47e4c60a6fc3308277b655cb5b48360522fe5
Nouha Rakeeb <Nouha_Rakeeb@hotmail.co.uk>
2021-09-26 01:47:52 +0100

    feat: added DELETE route

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,7 @@
+const { json } = require('express');
 const express = require('express');
 const router = express.Router();
-const readAndAppend = require('../helpers/readAndAppend');
+const helpers = require('../helpers/readAndAppend');
 const readFromFile = require('../helpers/readFromFile');
 
 const uuid = require('../helpers/uuid');
@@ -20,7 +21,7 @@ if (title && text) {
         id: uuid()
     };
 
-    readAndAppend(newNote, './db/db.json');
+    helpers.readAndAppend(newNote, './db/db.json');
 
     const response = {
         status: 'success',
@@ -35,7 +36,17 @@ if (title && text) {
 });
 
 router.delete('/:id', (req, res) => {
-    console.log("success!");
-})
+    let noteId = req.params.id;
+    readFromFile('./db/db.json').then((data) => {
+    const parsedNotes = JSON.parse(data);
+    parsedNotes.forEach(note => {
+           if(note.id == noteId) {
+               parsedNotes.splice(note, 1)
+               //parsedNotes.pop(indexof(note));
+           }  
+       });
+       helpers.writeToFile('./db/db.json', parsedNotes);
+    }
+)})
 
 module.exports = router;
